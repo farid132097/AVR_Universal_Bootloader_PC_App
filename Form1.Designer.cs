a58c3a69145b3d5bf87b8469891108b897956c62
farid132097 <faridmdislam@gmail.com>
2024-04-04 23:35:09 +0600

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace AVR_Universal_Bootloader_PC_App
+{
+    partial class AVR_Universal_Boot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cbSelectPort = new System.Windows.Forms.ComboBox();
+            this.UART = new System.IO.Ports.SerialPort(this.components);
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbSelectPort
+            // 
+            this.cbSelectPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSelectPort.FormattingEnabled = true;
+            this.cbSelectPort.Location = new System.Drawing.Point(12, 12);
+            this.cbSelectPort.Name = "cbSelectPort";
+            this.cbSelectPort.Size = new System.Drawing.Size(121, 21);
+            this.cbSelectPort.TabIndex = 0;
+            this.cbSelectPort.SelectedIndexChanged += new System.EventHandler(this.cbSelectPort_SelectedIndexChanged);
+            this.cbSelectPort.MouseClick += new System.Windows.Forms.MouseEventHandler(this.cbSelectPort_MouseClick);
+            // 
+            // UART
+            // 
+            this.UART.Handshake = System.IO.Ports.Handshake.XOnXOff;
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(12, 39);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(121, 23);
+            this.btnWrite.TabIndex = 1;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // AVR_Universal_Boot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 143);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.cbSelectPort);
+            this.Name = "AVR_Universal_Boot";
+            this.Text = "AVR Universal Boot";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbSelectPort;
+        private System.IO.Ports.SerialPort UART;
+        private System.Windows.Forms.Button btnWrite;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AVR_Universal_Bootloader_PC_App
+{
+    public partial class AVR_Universal_Boot : Form
+    {
+        public AVR_Universal_Boot()
+        {
+            InitializeComponent();
+        }
+
+        private void cbSelectPort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (UART.IsOpen)
+            {
+                try
+                {
+                    UART.Close();
+                }
+                catch
+                {
+
+                }
+            }
+
+            UART.PortName = cbSelectPort.SelectedItem.ToString();
+            UART.BaudRate = 9600;
+            try
+            {
+                UART.Open();
+            }
+            catch { 
+            
+            }
+
+            if (UART.IsOpen)
+            {
+                cbSelectPort.ForeColor = Color.LightGreen;
+            }
+            else
+            {
+                cbSelectPort.ForeColor = Color.Red;
+            }
+            
+
+        }
+
+        private void cbSelectPort_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (UART.IsOpen)
+            {
+                try
+                {
+                    UART.Close();
+                }
+                catch { 
+                    
+                }
+            }
+
+            string[] ports = SerialPort.GetPortNames();
+            cbSelectPort.Items.Clear();
+            cbSelectPort.Items.AddRange(ports);
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                UART.DtrEnable = true;
+                UART.RtsEnable = true;
+            }
+            catch
+            {
+
+            }
+            
+            Thread.Sleep(5);
+            try
+            {
+                UART.DtrEnable = false;
+                UART.RtsEnable = false;
+            }
+            catch
+            {
+
+            }
+            
+        }
+    }
+}
